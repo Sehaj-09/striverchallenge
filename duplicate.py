44b274780218583d8e1208ebd76c45ad530592d2
Sehaj-09 <54837609+Sehaj-09@users.noreply.github.com>
2022-06-06 23:57:28 +0530

    Create duplicate.py

---
+++ duplicate.py
@@ -0,0 +1,7 @@
+def findDuplicate(self, nums: List[int]) -> int:
+        n=set()
+        for i in nums:
+            if i in n:
+                return i
+            else:
+                n.add(i)
